613fbe270421bca328a958c1c2a7a732ce420793
JAJADELL <jiajaja@icloud.com>
2022-07-16 11:41:24 +0800

    2022-07-16

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import jingdong
 import taobao
 
+
 def main():
-    jd=jingdong.Tk_JingDong()
-    tb=taobao.Tk_TaoBao()
+    jd = jingdong.Tk_JingDong()
+    tb = taobao.Tk_TaoBao()
     while True:
         type = input('''
         输入运行任务：
         1、京东
         2、淘宝
         你的选择是（1/2）：''')
-        if type =="1":
+        if type == "1":
             jd.win()
-        elif type =="2":
+        elif type == "2":
             tb.win()
         else:
             print('输入错误，重新输入')
